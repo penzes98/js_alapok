2dc50bd9a807f847b57550946834df1c94b9a87f
Unknown <penzes98@gmail.com>
2018-02-14 14:58:49 +0100

    js_alap

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 /*jslint devel: true */
-/*global $, jQuery, alert */
+/*global $,jQuery, alert*/
 /*jslint indent: 2 */
 
 console.log("hello world");
+
+$('.image-popup').magnificPopup({
+   type: 'image',
+    gallery:{ enable: true }
+});
