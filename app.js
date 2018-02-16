dc3330aac3fc22ef0787effb761e240c66fc1ada
Unknown <penzes98@gmail.com>
2018-02-16 09:30:51 +0100

    js_alapok
    folytatás

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-/*jslint devel: true */
-/*global $,jQuery, alert*/
-/*jslint indent: 2 */
+/*jslint devel: true*/
+/*global $, jQuery, alert*/
+/*jslint indent: 2*/
 
-console.log("hello world");
 
-$('.image-popup').magnificPopup({
-   type: 'image',
-    gallery:{ enable: true }
-});
+(function () {
+  'use strict';
+  console.log("hello world");
+  $('.image-popup').magnificPopup({
+    type: 'image',
+      gallery: {
+          enabled: true
+      }
+  });
+}());
+//minden image-popup class-al rendelkező elemet elrejtünk
+//[].forEach.call(document.querySelectorAll('.image-popup'),
+//               function (el) {
+//    el.style.display = 'none' ;
+//});
+
+
+//ez ugaynazt csinálja mint a fentebbi kód, csak ez a jquery library segitségével:
+$('.image-popup').css('display', 'none');
+
